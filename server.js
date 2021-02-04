1c7dcc065457d462503e9cd04f872d1eb895625e
Logan Coates <coates.logan1125@gmail.com>
2021-02-03 21:58:38 -0700

    deploying

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const uri = process.env.MONGODB_URI;
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,10 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(uri || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  });
 
 // routes
 app.use(require("./routes/api.js"));
